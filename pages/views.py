bfe28ea6c0ad88ab6f4b5e512612cc20afb4b530
unknown <clases@M244-109.DIAXPS>
2022-10-20 10:34:15 -0700

    initial commit

---
+++ pages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+#pages/views.py
+from django.http import HttpResponse
+
+def homePageView(request):
+    return HttpResponse('Hello, World!')
+    
+# Create your views here.
